453920573c0032d3770bc23db91418d10c9c4a3f
tiddd <tiddd@indiana.edu>
2019-06-04 14:47:39 -0400

    Index - include path is different on remote server

--- public/index.php
+++ public/index.php
@@ -1,13 +1,16 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../helpers.php';
-require_once __DIR__ . '/../model/config.php';
+if(strpos($_SERVER['HTTP_HOST'], "localhost") !== false) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+    require_once __DIR__ . '/../helpers.php';
+    require_once __DIR__ . '/../model/config.php';
+    require_once __DIR__ . '/../routes.php';
+}
+else {
+    require_once __DIR__ . '/../gpra-app/vendor/autoload.php';
+    require_once __DIR__ . '/../gpra-app/helpers.php';
+    require_once __DIR__ . '/../gpra-app/model/config.php';
+    require_once __DIR__ . '/../gpra-app/routes.php';
+}
 
 use Pecee\SimpleRouter\SimpleRouter as Router;
-use Pecee\Http\Url;
-use Pecee\Http\Response;
-use Pecee\Http\Request;
-
-require_once '../routes.php';
-
 Router::start();
