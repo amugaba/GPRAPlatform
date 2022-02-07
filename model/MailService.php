85dd328fe238bed921a23e07cda5ce0053f13a9d
David <tiddd@indiana.edu>
2022-02-07 16:36:18 -0500

    Deleting last commit

--- model/MailService.php
+++ model/MailService.php
@@ -88,7 +88,7 @@ class MailService {
         $mail->Host = "smtp.dreamhost.com";
         $mail->Port = 587;
         $mail->Username = 'david.tidd@angstrom-software.com';
-        $mail->Password = 'tiny emu riding camel';
+        $mail->Password = 'tiny emu riding camel2';
 
         $mail->CharSet = 'UTF-8';
         $mail->setFrom('david.tidd@angstrom-software.com', 'Angstrom Software Support');
